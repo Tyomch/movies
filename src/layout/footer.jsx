42b036290b9196fe4bd76363978fbab4481444a6
Tyomch <artem.spirin66@gmail.com>
2023-06-14 06:28:31 +0300

    tigt

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
     <div className="footer-copyright">
       <div className='container'>
         {new Date().getFullYear}
-        <img src={require('../photos/trailer.png')} alt="#" width="100px" height="100px"/>
+        <img src={require('../photos/free-sticker-birthday-cake-4213726.png')} alt="#" width="100px" height="100px"/>
         <a href="#!" className="max grey-text text-lighten-4 right">
           Spirin A.
         </a>
